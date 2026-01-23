ea2c22b9f6cac5c5f4d22a92da38b9fe556dd497
Kellman Ethan <J5.SC@pcsb.org>
2026-01-23 09:44:12 -0500

    add file

---
+++ main.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+typedef enum {
+    EVENT_PRINTF
+} EventType;
+
+typedef struct {
+    EventType type;
+    int value;
+} Event;
+
+#define MAX_EVENTS 128
+Event eventLog[MAX_EVENTS];
+int eventCount = 0;
+
+// game-side: add event to log
+void log_event(Event e) {
+    if (eventCount < MAX_EVENTS)
+        eventLog[eventCount++] = e;
+}
+
+// engine-side: apply ONE event
+void engine_apply_event(Event e) {
+    if (e.type == EVENT_PRINTF) {
+        printf("%d!\n", e.value);
+    }
+}
+
+// engine-side: replay ALL events
+void engine_replay() {
+    for (int i = 0; i < eventCount; i++) {
+        engine_apply_event(eventLog[i]);
+    }
+}
+
+int main() {
+    // game creates events
+    log_event((Event){ EVENT_PRINTF, 10 });
+    log_event((Event){ EVENT_PRINTF, 20 });
+    log_event((Event){ EVENT_PRINTF, 30 });
+
+    // engine replays them
+    engine_replay();
+
+    return 0;
+}
